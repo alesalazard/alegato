c94b979149411c49a3388ebcfb07df81e387457f
Alexandretta <alxsalazar21@gmail.com>
2025-11-25 10:26:53 -0500

    Se varia la ruta absoluta de imagenes para renderizar en github pages

--- sections/ocupacionale.js
+++ sections/ocupacionale.js
@@ -1,7 +1,7 @@
 export function renderOcupacionale() {
   return `
     <p class="product-text">Tengo más de 20 años de experiencia como terapeuta ocupacional, trabajé con dificultades del aprendizaje, neuroestimulación y salud mental.
-    Promuevo experiencias que sanan y conectan con nuestras rutinas. Puedes seguirme en Instagram: <a href="https://www.instagram.com/ocupacionale/" target="_blank"  ><img src="/img/ig.png" class="icon"/></a></p>
+    Promuevo experiencias que sanan y conectan con nuestras rutinas. Puedes seguirme en Instagram: <a href="https://www.instagram.com/ocupacionale/" target="_blank"  ><img src="img/ig.png" class="icon"/></a></p>
     <img src="./img/ocupacionale.png" alt="Ocupacionale" class="img-section" />
   `;
 }
